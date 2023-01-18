032c67a82f095df4bbe377a967dd2c666b5a6cae
a1234jehad <jehad56@hotmail.com>
2023-01-18 07:51:45 +0300

    Adding files

--- projects/models.py
+++ projects/models.py
@@ -5,8 +5,12 @@ import uuid
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True,blank=True)
+    featured_image = models.ImageField(null=True,blank=True,default="default.jpg")
     demo_link = models.CharField(max_length=2000,null=True,blank=True)
     source_link = models.CharField(max_length=2000,null=True,blank=True)
+    tags = models.ManyToManyField('Tag',blank=True)
+    vote_totals = models.IntegerField(default=0,null=True,blank=True)
+    vote_ratio = models.IntegerField(default=0,null=True,blank=True)
     created = models.DateTimeField(auto_now_add=True) # date and time stamp
     id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
 
@@ -28,3 +32,12 @@ class Review(models.Model):
     id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
     def __str__(self):
         return self.value
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True) # date and time stamp
+    id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
+    
+    def __str__(self):
+        return self.name
+        
--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
 
 # Create your views here.
 def projects(req):
-    msg = 'Hello, you are on the projects page'
-    number = 101 
-    context = {'message':msg,
-                'number':number}
+    projects = Project.objects.all()
+    print(projects)
+    context = {'projects': projects}
     return render(req,'projects/projects.html',context)
 
 def project(req,pk):
-    return render(req,'projects/single-project.html')
+    proj = Project.objects.get(id=pk)
+    context = {'projectObj' : proj}
+    return render(req,'projects/single-project.html',context)
 
+def createProject(req):
+    form = ProjectForm()
+
+    if req.method == 'POST':
+        form = ProjectForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form' : form}
+    return render(req,"projects/project_form.html",context)
+
+
+def updateProject(req,pk):
+    # pk = primery key
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if req.method == 'POST':
+        form = ProjectForm(req.POST,instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form' : form}
+    return render(req,"projects/project_form.html",context)
+
+def deleteProject(req,pk):
+    project = Project.objects.get(id=pk)
+    if req.method == 'POST':
+        project.delete()
+        return redirect('projects')
+        
+    context = {'obj': project}
+    return render(req,'projects/deleted_obj.html',context)
